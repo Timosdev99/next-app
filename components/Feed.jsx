f4dd63720f8098af8f6a99c64c7841253d2dc480
timosdev99 <timothyisah4@gmail.com>
2024-11-22 19:11:17 +0100

    add

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -28,7 +28,7 @@ const Feed = () => {
 
   const fetchPosts = async () => {
     const response = await fetch("/api/prompt");
-    const data = await response.json();
+    const data = await response.json();  
 
     setAllPosts(data);
   };
